4ade3c5506099f19f1a17e256f0513c43332812e
sharduldabhane <shardul.dabhane@gmail.com>
2024-03-08 12:16:41 -0500

    Updated app.py. Added mysql library, updated connection to rds db on AWS

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,9 @@
-# backend/app.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 import requests
 from dotenv import load_dotenv
-import mysql.connector
+import pymysql
 
 load_dotenv()
 
@@ -37,6 +36,28 @@ def create_plan():
             'max_tokens': 2048
         }
     )
+
+    # Attempt to connect to the database using pymysql
+    conn = get_db_connection()
+    if conn is not None:
+        print("Connected to db!")
+
+        cursor = conn.cursor()
+
+        # Execute a SQL query
+        cursor.execute('SELECT * FROM Roles')
+
+        # Fetch the results
+        results = cursor.fetchall()
+
+        #  Print the results
+        for result in results:
+            print(result)
+        conn.close()
+
+
+    else:
+        print("Failed to connect to the database.")
     print(response)
     return jsonify(response.json())
 
@@ -44,6 +65,21 @@ def create_plan():
 def analytics():
     return jsonify({'study_plan_count': study_plan_count})
 
+# Database connection function updated for pymysql
+def get_db_connection():
+    try:
+        conn = pymysql.connect(
+        host=os.getenv('DB_HOST'),  # e.g., 'dev-backend.cjewkge6mhd5.us-east-1.rds.amazonaws.com'
+        user=os.getenv('DB_USER'),  # e.g., 'admin'
+        password=os.getenv('DB_PASSWORD'),  # e.g., 'parallel'
+        database=os.getenv('DB_NAME'),  # Your database name
+        port=3306
+        )
+        #print("Connected to db!")
+        return conn
+    except Exception as e:
+        print(f"Failed to connect to db: {e}")
+        return None
 
 if __name__ == '__main__':
     app.run(debug=True)

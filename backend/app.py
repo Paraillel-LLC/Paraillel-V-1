2f92b7941e42ee59d30e0363fde54888c8039e97
mohan <mohansrisaimahi333@gmail.com>
2024-03-08 14:22:38 -0500

    app.py changes

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,24 @@
-# backend/app.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 import requests
 from dotenv import load_dotenv
 import mysql.connector
-
 load_dotenv()
+import boto3
+
 
 app = Flask(__name__)
 CORS(app)
-
-
 # Counter for analytics
 study_plan_count = 0
-
 @app.route('/')
 def index():
     return "Flask server is running!"
-
 @app.route('/create-plan', methods=['POST'])
 def create_plan():
     global study_plan_count
     study_plan_count += 1
-
     data = request.json
     prompt = data.get('prompt')
     headers = {
@@ -37,13 +32,46 @@ def create_plan():
             'max_tokens': 2048
         }
     )
+    # Attempt to connect to the database
+    
+    conn = get_db_connection()
+    if conn is not None:
+        print("Connected to db!")
+        #conn.close()
+    else:
+        print("Failed to connect to the database.")
     print(response)
     return jsonify(response.json())
-
 @app.route('/analytics', methods=['GET'])
 def analytics():
     return jsonify({'study_plan_count': study_plan_count})
-
-
+# Database connection function
+def get_db_connection():
+    try:
+        session = boto3.Session(region_name='us-east-1')
+        client = session.client('ssm')
+        instance_id = 'i-0de9186fc24cd6fbb'
+    
+        response = client.start_session(
+        Target=instance_id
+        )
+    
+        session_id = response['SessionId']
+        token = response['Token']
+    
+        print("Session ID:", session_id)
+        print("Token:", token)
+        print("Connected to db!")
+       # conn = mysql.connector.connect(
+       #     host=os.getenv('DB_HOST'),  # e.g., 'dev-backend.cjewkge6mhd5.us-east-1.rds.amazonaws.com'
+       #     user=os.getenv('DB_USER'),  # e.g., 'admin'
+       #     password=os.getenv('DB_PASSWORD'),  # e.g., 'paraillel'
+       #     database=os.getenv('DB_NAME')  # Your database name
+       # )
+        print("Connected to db!")
+        return session
+    except Exception as e:
+        print(f"Failed to connect to db: {e}") 
+        return None
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\IUB Sem 4\Paraillel\SecondProgram\paraillel-dashboard-app\backend\venv
+set VIRTUAL_ENV=C:\Users\mohan\OneDrive\Desktop\Paraillel-V-1\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
